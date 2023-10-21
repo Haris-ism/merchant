ed77071fdd75814fe2162ae97ee8fc2eb3d845b0
Haris <amin.haris@ottodigital.id>
2023-10-21 18:12:36 +0700

    bug fix trans

--- databases/postgresql/trans.postgre.go
+++ databases/postgresql/trans.postgre.go
@@ -9,7 +9,7 @@ import (
 
 func (db *postgreDB)CheckItems(req cModels.ReqTransItem)(models.Items,error){
 	reqDB:=models.Items{}
-	err:=db.postgre.Select("merchant_items.ID, merchant_items.name as item_name, merchant_items.type, merchant_items.price, merchant_discounts.percentage, merchant_discounts.name as discount_name").
+	err:=db.postgre.Select("merchant_items.ID, merchant_items.item_name , merchant_items.type, merchant_items.price, merchant_discounts.percentage, merchant_discounts.name as discount_name").
 		Joins("left join merchant_discounts on merchant_discounts.type = merchant_items.type").
 		Where("merchant_items.id = ? and merchant_discounts.name = ?",req.ID,req.Discount).Find(&reqDB).Error
 	if err!=nil{
